76d2d5f8cc31d4b53400a68191280e448f1f98f3
JAFAJ0 <86894074+JAFAJ0@users.noreply.github.com>
2021-10-17 22:56:08 -0400

    Update Profile.js
    Change username to userid and it looks better

--- server/models/Profile.js
+++ server/models/Profile.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
 const profileSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
+    userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
     },
     firstName: {
         type: String,
