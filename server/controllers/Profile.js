e03aa6d77e95130579905d410659bcf1673985e9
JAFAJ0 <86894074+JAFAJ0@users.noreply.github.com>
2021-10-19 21:49:20 -0400

    Controller and route added for profile&availability #6 (#46)
    * Update pure profile model to store user profile and user availability
    
    Two databases, one is "one to one", use username to identify. Can only have one identical username.
    The other, "many to many".  Can have the same username for many records.
    
    Rename that parameter to camel case and add photo URL for profile photo future functions
    
    * Update Profile.js
    
    * Provide default date for birthdate
    
    * remove unused crypt and name
    
    
    Add empty string as default opinions to those unrequired parts
    
    * change username to userid & remove unused default 
    
    & add required identify
    
    * update profile availabilities model and controls
    
    * Fix issue in names & remove unused next
    
    * remove email-already inside user
    Add check that user can only edit user`s availability

---
+++ server/controllers/Profile.js
@@ -0,0 +1,72 @@
+const mongoose = require("mongoose");
+const Profile = require("../Models/Profile");
+const asyncHandler = require("express-async-handler");
+
+// @route GET /profile
+// @desc the profile of the relevant user
+// @access Private
+exports.getProfile = asyncHandler(async (req, res) => {
+    const userId = req.user.id;
+
+    const profile = await Profile.findOne({ userId });
+
+    res.send({ profile });
+});
+
+// @route POST /profile
+// @desc Create a new profile
+// @access Private
+exports.createProfile = asyncHandler(async (req, res) => {
+    const userId = req.user.id;
+
+    const { firstName, lastName, gender, birthDate, phoneNum, address, description } = req.body;
+
+    if (!firstName || !lastName || !phoneNum) {
+        res.status(400);
+        throw new Error("Incomplete profile field");
+    }
+
+    const phoneNumExists = await Profile.findOne({ phoneNum });
+
+    if (phoneNumExists) {
+        res.status(400);
+        throw new Error("The phone number is already in use");
+    }
+
+    const profile = {
+        userId: userId,
+        firstName: firstName,
+        lastName: lastName,
+        gender:gender,
+        birthDate:birthDate,
+        phoneNum: phoneNum,
+        address:address,
+        description:description
+    };
+
+    const resp = await Profile.create(profile);
+
+    res.status(201).json(resp);
+});
+
+// @route PATCH /Profile
+// @desc Update profile with approved or decline
+// @access Private
+exports.updateProfile = asyncHandler(async (req, res) => {
+    const userId = req.user.id;
+
+    const { profileUpdates } = req.body;
+
+    const profile = await Profile.findById(userId);
+
+    if (!profile) {
+        res.status(404);
+        throw new Error("User id do not exist! Error happened!");
+    }
+
+    profile.status = profileUpdates;
+
+    await profile.save();
+
+    res.status(200).json(profile);
+});
---
+++ server/routes/Availability.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const protect = require("../middleware/auth");
+const {
+    getAvailability,
+    createAvailability,
+    updateAvailability,
+} = require("../controllers/Availability");
+
+router.route("/").get(protect, getAvailability);
+
+router.route("/").post(protect, createAvailability);
+
+router.route("/:id").patch(protect, updateAvailability);
+
+module.exports = router;
