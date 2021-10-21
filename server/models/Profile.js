11af6e066dcc4c0d931ec26cb3d11ca79f045f5e
JA <86894074+JAFAJ0@users.noreply.github.com>
2021-10-21 00:11:05 -0400

    Bring the files in main back to this branch

--- server/models/Profile.js
+++ server/models/Profile.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 
 const profileSchema = new mongoose.Schema({
     userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true,
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
     },
     firstName: {
         type: String,
