56778444298dc92ad1a34a8da5fb8075934bede3
JA <86894074+JAFAJ0@users.noreply.github.com>
2021-10-24 22:43:10 -0400

    Creat mock pages for profileskeleton. And only for testing and displaying, Change App display

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,11 @@
 import { MuiThemeProvider } from '@material-ui/core';
 import { theme } from './themes/theme';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
-import Welcome from './pages/Welcome/Welcome';
 import Login from './pages/Login/Login';
 import Signup from './pages/SignUp/SignUp';
-import ProfilePhotoMock from './pages/ProfileSkeleton/ProfilePhotoMock/ProfilePhotoMock';
+import ProfileEditionMock from './pages/ProfileSkeleton/ProfileEditionMock/ProfileEditionMock';
 import Dashboard from './pages/Dashboard/Dashboard';
-import NavBar from './components/Navbar/NavBar';
-import ProfileListings from './components/ProfileListings/ProfileListings';
-import ProfileDetails from './components/ProfileDetails/ProfileDetails';
+import MySitters from './pages/MySitters/MySitters';
 import { AuthProvider } from './context/useAuthContext';
 import { SocketProvider } from './context/useSocketContext';
 import { SnackBarProvider } from './context/useSnackbarContext';
@@ -26,31 +23,14 @@ function App(): JSX.Element {
           <AuthProvider>
             <SocketProvider>
               <Switch>
-                <Route exact path="/">
-                  <Welcome />
-                </Route>
-                <Route exact path="/login">
-                  <NavBar elevation={16} color="inherit" />
-                  <Login />
-                </Route>
-                <Route exact path="/signup">
-                  <NavBar elevation={16} color="inherit" />
-                  <Signup />
-                </Route>
-                <Route exact path="/profiles">
-                  <NavBar elevation={16} color="inherit" />
-                  <ProfileListings />
-                </Route>
-                <Route path="/profiles/:profileId">
-                  <NavBar elevation={16} color="inherit" />
-                  <ProfileDetails />
-                </Route>
-                <Route path="/dashboard">
-                  <NavBar elevation={16} color="inherit" />
-                  <Dashboard />
+                <Route exact path="/profileEditionMock" component={ProfileEditionMock} />
+                <Route exact path="/signup" component={Signup} />
+                <Route exact path="/dashboard">
+                  {/* <Dashboard /> disabled until Navbar setup */}
+                  <MySitters />
                 </Route>
                 <Route path="*">
-                  <Redirect to="/" />
+                  <Redirect to="/profileEditionMock" />
                 </Route>
               </Switch>
             </SocketProvider>
