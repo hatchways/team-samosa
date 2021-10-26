a2bc823bc2c873d1e6de7ee7872db740e0429266
Robert Reinert <robert.reinert@gmail.com>
2021-10-26 13:37:42 -0700

    fixed Container scroll bar issue

--- client/src/components/Profiles/Profiles.tsx
+++ client/src/components/Profiles/Profiles.tsx
@@ -1,4 +1,5 @@
 import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
 import Container from '@material-ui/core/Container';
 import useStyles from './useStyles';
 import ProfileRequest from './ProfileRequest/ProfileRequest';
@@ -8,15 +9,17 @@ export default function Profiles(): JSX.Element {
   const classes = useStyles();
 
   return (
-    <Container>
-      <Grid className={classes.root} container spacing={10} alignContent="center">
-        <Grid item xs={12} md={7}>
-          <ProfileMain />
+    <Box p={3}>
+      <Container>
+        <Grid className={classes.root} container spacing={10} alignContent="center">
+          <Grid item xs={12} md={7}>
+            <ProfileMain />
+          </Grid>
+          <Grid item xs={12} md={5}>
+            <ProfileRequest />
+          </Grid>
         </Grid>
-        <Grid item xs={12} md={5}>
-          <ProfileRequest />
-        </Grid>
-      </Grid>
-    </Container>
+      </Container>
+    </Box>
   );
 }
