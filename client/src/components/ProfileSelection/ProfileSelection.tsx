8bf760f42633086b526959757d5ca3c628196f0b
JA <86894074+JAFAJ0@users.noreply.github.com>
2021-10-28 07:27:52 -0400

    address changes: same code reduction

--- client/src/components/ProfileSelection/ProfileSelection.tsx
+++ client/src/components/ProfileSelection/ProfileSelection.tsx
@@ -5,18 +5,16 @@ import Button from '@material-ui/core/Button';
 
 const ChatSideBanner = (props: { name: string; link: string; selected: boolean }): JSX.Element => {
   const classes = useStyles();
-  return props.selected ? (
+  return (
     <Grid container>
       <Grid item xs>
-        <Button component={Link} to={props.link} color="inherit" className={classes.accBtnselected} variant="contained">
-          {props.name}
-        </Button>
-      </Grid>
-    </Grid>
-  ) : (
-    <Grid container>
-      <Grid item xs>
-        <Button component={Link} to={props.link} color="inherit" className={classes.accBtn} variant="contained">
+        <Button
+          component={Link}
+          to={props.link}
+          color="inherit"
+          className={props.selected ? classes.accBtnselected : classes.accBtn}
+          variant="contained"
+        >
           {props.name}
         </Button>
       </Grid>
