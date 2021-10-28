f60d25ff7cb454370b994e7b08f44f51cacc6ba8
JA <86894074+JAFAJ0@users.noreply.github.com>
2021-10-27 23:38:17 -0400

    merge profile selection and do front end updates

--- client/src/pages/ProfileSkeleton/ProfilePhoto/useStyles.ts
+++ client/src/pages/ProfileSkeleton/ProfilePhoto/useStyles.ts
@@ -4,21 +4,18 @@ const useStyles = makeStyles(() => ({
   root: {
     minHeight: '100vh',
     '& .MuiInput-underline:before': {
-      borderBottom: '1.2px solid rgba(0, 0, 0, 0.2)',
+      borderBottom: '0.8px solid rgba(0, 0, 0, 0.2)',
     },
-    marginTop: '2%',
-    bbackgroundColor: '#000000',
+    marginTop: '5%',
   },
   authWrapper: {
     display: 'flex',
-    alignItems: 'flex-start',
+    alignItems: 'center',
     justifyContent: 'space-between',
     flexDirection: 'column',
-    minHeight: '100vh',
-    paddingTop: 23,
+    paddingTop: 35,
   },
   back: {
-    color: 'white',
     backgroundColor: '#fafafa',
   },
   welcome: {
@@ -27,6 +24,7 @@ const useStyles = makeStyles(() => ({
     color: '#000000',
     fontWeight: 700,
     fontFamily: "'Open Sans'",
+    textAlign: 'center',
   },
 }));
 
