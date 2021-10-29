ca9e4c47c753ef720a91e5c493bfcd907ce57113
Robert Reinert <robert.reinert@gmail.com>
2021-10-28 18:18:45 -0700

    fixed typo

--- client/src/helpers/APICalls/getProfile.ts
+++ client/src/helpers/APICalls/getProfile.ts
@@ -1,6 +1,6 @@
 import { FetchOptions } from '../../interface/FetchOptions';
 import { ProfileData } from '../../interface/Profile';
-export async function getProfile(id:string): Promise<ProfileData> {
+export async function getProfile(id: string): Promise<ProfileData> {
   const fetchOptions: FetchOptions = {
     method: 'GET',
     credentials: 'include',
