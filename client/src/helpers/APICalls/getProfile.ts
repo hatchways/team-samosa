5d2e33e10af5f60e26f6b80d87d829749a578b2e
Robert Reinert <robert.reinert@gmail.com>
2021-10-31 22:05:56 -0700

    getProfile fix

--- client/src/helpers/APICalls/getProfile.ts
+++ client/src/helpers/APICalls/getProfile.ts
@@ -1,11 +1,11 @@
 import { FetchOptions } from '../../interface/FetchOptions';
 import { ProfileApiData } from '../../interface/Profile';
-export async function getProfile(profileId?: string | ''): Promise<ProfileApiData> {
+export async function getProfile(): Promise<ProfileApiData> {
   const fetchOptions: FetchOptions = {
     method: 'GET',
     credentials: 'include',
   };
-  const res = await fetch(`/profile/${profileId}`, fetchOptions);
+  const res = await fetch(`/profile`, fetchOptions);
   const resp = await res.json();
   if (resp.error) {
     return resp;
