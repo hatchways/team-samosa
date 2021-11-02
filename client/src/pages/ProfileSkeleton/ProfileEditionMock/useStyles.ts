089b149525aa6bc278e3e8021fb8fe3beafba321
jingfan ai <86894074+JAFAJ0@users.noreply.github.com>
2021-11-01 21:06:51 -0400

    Fe profile settings skeleton #25 (#53)
    * Add protected Route component
    
    * Creat mock pages for profileskeleton. And only for testing and displaying, Change App display
    
    * add missing options
    
    * Rebased this branch, add components to jump between pages; edit styles
    
    * address changes: same code reduction
    
    * creat mocks and adress minor changes
    
    * fix typo
    
    * Fix import to fix nav issue
    
    * Fix active ClassName issue and remove unused inputs

---
+++ client/src/pages/ProfileSkeleton/ProfileEditionMock/useStyles.ts
@@ -0,0 +1,34 @@
+import { makeStyles } from '@material-ui/core/styles';
+import { theme } from '../../../themes/theme';
+
+const useStyles = makeStyles(() => ({
+  root: {
+    minHeight: '100vh',
+    '& .MuiInput-underline:before': {
+      borderBottom: '1.2px solid rgba(0, 0, 0, 0.2)',
+    },
+    marginTop: '2%',
+    bbackgroundColor: '#000000',
+  },
+  authWrapper: {
+    display: 'flex',
+    alignItems: 'flex-start',
+    justifyContent: 'space-between',
+    flexDirection: 'column',
+    minHeight: '100vh',
+    paddingTop: 23,
+  },
+  back: {
+    color: 'white',
+    backgroundColor: '#fafafa',
+  },
+  welcome: {
+    fontSize: 26,
+    paddingBottom: 20,
+    color: '#000000',
+    fontWeight: 700,
+    fontFamily: theme.typography.body2.fontFamily,
+  },
+}));
+
+export default useStyles;
